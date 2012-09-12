504cc5225f9983e597c31cb3dfd7a822166c18b5
sohesado <sohesado@thinkii>
2012-09-13 01:42:57 +0300

    Implemented application pull function.

--- src/com/jolive/ui/JOliveActivity.java
+++ src/com/jolive/ui/JOliveActivity.java
@@ -9,42 +9,93 @@ import uk.ac.rdg.resc.jstyx.client.StyxConnection;
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.HorizontalScrollView;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 
 public class JOliveActivity extends Activity {
 	Handler ooHandler;
+	CStyxFile root;
+	View rscreen;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ConfigureLog4j.configure();
-        
-        ScrollView vRootComp = new ScrollView(this);
-        vRootComp.addView(init("192.168.2.4", 4987)); 
-        HorizontalScrollView hRootComp = new HorizontalScrollView(this);
-        hRootComp.addView(vRootComp);
-        setContentView(hRootComp);
+		StyxConnection conn = new StyxConnection("192.168.1.5", 4987);
+		try {
+			conn.connect();
+		} catch (StyxException e) {
+			System.err.println("Could not reach PC");
+			e.printStackTrace();
+		}
+		root = conn.getRootDirectory();
+		ooHandler = new Handler();
+		(new OOlive(root.getFile("olive"), ooHandler)).start();
+		rscreen = initScreen(root.getFile("main"));
+        setContentView(rscreen);
     }
     
-    ViewGroup init(String ip, int port) {
-		FrameLayout rootComp = new FrameLayout(this);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public boolean onOptionsItemSelected (MenuItem item) {
+    	CStyxFile appl = root.getFile("appl");
+    	CStyxFile[] apps = new CStyxFile[0];
 		try {
-			StyxConnection conn = new StyxConnection(ip, port);
-			conn.connect();		
-			CStyxFile root = conn.getRootDirectory();
-			ooHandler = new Handler();
-			(new OOlive(root.getFile("olive"), ooHandler)).start();
-			CStyxFile main = root.getFile("main");
-			JOPanel jp = new JOPanel(main, rootComp);
-			jp.createUITree(main, rootComp);
+			apps = appl.getChildren();
 		} catch (StyxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return rootComp;
+    	String[] appPaths = new String[apps.length];
+    	boolean[] checked = new boolean[apps.length];
+		for (int i = 0; i < apps.length; i++) {
+			appPaths[i] = apps[i].getPath();
+		}
+
+		PullAppListener pl = new PullAppListener(appPaths, checked, this.root, this);
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMultiChoiceItems(appPaths, checked, pl);
+		builder.setCancelable(false);
+	    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+	           public void onClick(DialogInterface dialog, int id) {
+	                dialog.cancel();
+	           }
+	       });
+	    builder.setPositiveButton("Pull", pl);
+		AlertDialog alert = builder.create();
+		alert.show();
+    	return true;
+    }
+
+    public ViewGroup initScreen(CStyxFile screen) {
+		FrameLayout rootComp = new FrameLayout(this);
+        ScrollView vRootComp = new ScrollView(this);
+        vRootComp.addView(rootComp); 
+        HorizontalScrollView hRootComp = new HorizontalScrollView(this);
+        hRootComp.addView(vRootComp);
+
+		try { 
+			JOPanel jp = new JOPanel(screen, rootComp);
+			jp.createUITree(screen, rootComp);
+		} catch (StyxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return hRootComp;
     }
 }
---
+++ src/com/jolive/ui/PullAppListener.java
@@ -0,0 +1,59 @@
+package com.jolive.ui;
+
+import com.jolive.omero.OOlive;
+
+import uk.ac.rdg.resc.jstyx.StyxException;
+import uk.ac.rdg.resc.jstyx.StyxUtils;
+import uk.ac.rdg.resc.jstyx.client.CStyxFile;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
+
+public class PullAppListener implements OnMultiChoiceClickListener,
+										OnClickListener {
+	private String[] paths;
+	private boolean[] checked;
+	private CStyxFile root;
+	private JOliveActivity activity;
+
+	public PullAppListener(String[] paths, boolean[] checked, CStyxFile root, JOliveActivity activity) {
+		this.paths = paths;
+		this.checked = checked;
+		this.root = root;
+		this.activity = activity;
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		CStyxFile myScreen = root.getFile("android");
+		try {
+			myScreen.openOrCreate(true, StyxUtils.OREAD);
+		} catch (StyxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for (int i = 0; i < paths.length; i++) {
+			if (checked[i]) {
+				CStyxFile app = root.getFile(paths[i]);
+				try {
+					CStyxFile ctl = app.openFile("ctl", StyxUtils.OWRITE | StyxUtils.ORCLOSE);
+					ctl.write("copyto /android/row:wins/col:2".getBytes(), 0, false);
+				} catch (StyxException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		activity.setContentView(activity.initScreen(myScreen));
+
+	}
+
+	@Override
+	public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+		checked[which] = isChecked;
+	}
+
+}
